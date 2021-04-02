a131343cbedd992369e92e3cb2598569e41137f4
Brandon Nelson <bnel1201@gmail.com>
2021-04-02 16:16:57 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,9 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "src"},
+    packages = ['convert_notes']
+    package_dir={'convert_notes': 'src/convert_notes'},
+    package_data={'convert_notes': ['pandoc.css']}
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
     requires=["fire"]
